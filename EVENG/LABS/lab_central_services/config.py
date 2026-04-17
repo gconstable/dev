9155eca9cbaaa643a928b8453d38e6ea872da9eb
Gary Constable <40803397+gconstable@users.noreply.github.com>
2026-04-17 17:42:14 +0100

    Update config.py

--- EVENG/LABS/lab_central_services/config.py
+++ EVENG/LABS/lab_central_services/config.py
@@ -2,8 +2,19 @@
 LAB_NAME = "lab_central_services"
 
 # LAB_DATA
-lab = {
-  "name": ("Jenkins_Auto_Lab_" + LAB_NAME), 
-  "description": "Lab created via Jenkins CI/CD", 
-  "path": "/"
-}
+lab = 
+  {
+    "name": ("Jenkins_Auto_Lab_" + LAB_NAME), 
+    "description": "Lab created via Jenkins CI/CD", 
+    "path": "/"
+  }
+
+# OOB_MGMT
+mgmt_cloud = 
+  {
+    "name": "oob_mgmt", 
+    "network_type": 
+    "pnet0", 
+    "left": 300, 
+    "top": 400
+  }
