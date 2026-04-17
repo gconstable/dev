85d3cf909cfb055dfaffecfa14d4e1d3b1d4f2b2
Gary Constable <40803397+gconstable@users.noreply.github.com>
2026-04-17 17:36:37 +0100

    Update config.py

--- EVENG/LABS/lab_central_services/config.py
+++ EVENG/LABS/lab_central_services/config.py
@@ -1,1 +1,9 @@
+## LAB_CONFIG
+LAB_NAME = "lab_central_services"
 
+# LAB_DATA
+lab = {
+  "name": ("Jenkins_Auto_Lab_" + LAB_NAME), 
+  "description": "Lab created via Jenkins CI/CD", 
+  "path": "/"
+}
