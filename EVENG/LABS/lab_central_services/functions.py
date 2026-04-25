9e15a9935c831a0284268e89227008c84d31ce6a
Gary Constable <40803397+gconstable@users.noreply.github.com>
2026-04-25 12:57:18 +0100

    updates
    - Added new eve addresses.
    - Added new functions file to labs for deployment

---
+++ EVENG/LABS/lab_central_services/functions.py
@@ -0,0 +1,72 @@
+import json
+
+def get_api_command_node(data):
+    # Convert incoming keys to a set for easy comparison
+    incoming_keys = set(data.keys())
+
+    # Define your "fingerprints" for each action
+    actions = {
+        "ADD": 
+            {
+                "template",
+                "delay",
+                "name",
+                "node_type",
+                "top",
+                "left",
+                "console",
+                "config",
+                "ethernet",
+                "serial",
+                "image",
+                "icon",
+                "ram",
+                "cpu",
+                "nvram",
+                "idlepc",
+                "slot"
+            },
+        "UPDATE": {"id", "field", "value"},
+        "DELETE": {"id"}
+    }
+
+    for command, required_keys in actions.items():
+        if incoming_keys == required_keys:
+            return command
+            
+    return "UNKNOWN_ACTION"
+
+def get_api_command_n2c(data):
+    # Convert incoming keys to a set for easy comparison
+    incoming_keys = set(data.keys())
+
+    # Define your "fingerprints" for each action
+    actions = {
+        "ADD": {"src", "src_label", "dst", "dst_label", "media"},
+        "UPDATE": {"id", "field", "value"},
+        "DELETE": {"id"}
+    }
+
+    for command, required_keys in actions.items():
+        if incoming_keys == required_keys:
+            return command
+            
+    return "UNKNOWN_ACTION"
+
+def get_api_command_n2n(data):
+    # Convert incoming keys to a set for easy comparison
+    incoming_keys = set(data.keys())
+
+    # Define your "fingerprints" for each action
+    actions = {
+        "ADD": {"src", "src_label", "dst", "dst_label", "media"},
+        "UPDATE": {"id", "field", "value"},
+        "DELETE": {"id"}
+    }
+
+    for command, required_keys in actions.items():
+        if incoming_keys == required_keys:
+            return command
+            
+    return "UNKNOWN_ACTION"
+
